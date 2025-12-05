69e55cc3c33d2233cfb910d1cdd374dcc98e2d9a
avan <leechunyin2004@gmail.com>
2025-12-05 13:10:18 +0000

    oops

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use("/api/logs", logsRoutes)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => { console.log("- Connected to MongoDB") })
   .catch((error) => { console.error("Error connecting to MongoDB:", error) })
-  .finally(() => User.insertOne({ user: "avan", APIKEY: "9R2ovBvhV0pa8R4V/VAYkDjUqFI7VoRDRWxYVpvm+3ui/dswnbiGin598RI6zX47+MPwVNujwagNSJRtfxfxm5VvZ1b1QhRkd/8O2p72SyQ7IZyyd+l7dPTwmCYcTs3c", creationDate: new Date() }))
 
 app.listen(port, () => {
   console.log(`- Listening on http://localhost:${port}`)
